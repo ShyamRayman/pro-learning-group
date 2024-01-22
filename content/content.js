68d3f7c9cbeaf109e10909cf5af09d9e3cb7d972
ShyamRayman <143590963+ShyamRayman@users.noreply.github.com>
2024-01-22 20:18:18 +0530

    Add files via upload

---
+++ content/content.js
@@ -0,0 +1,138 @@
+import React from "react";
+// import 'content.css';
+
+function Content(){
+    return(
+        <>
+        <div className="text-left col-lg-12 container">
+        <h3 className="text-left col-lg-12 container">Course content</h3>
+        <p className="text-left col-lg-12 container">34 sections . 503 lectures . 36h 33m total length</p>
+       
+
+<div class="accordion w-100 mt-5 col-lg-10 container text-left" id="accordionPanelsStayOpenExample">
+  
+  
+  <div class="accordion-item text-left">
+    <h2 class="accordion-header">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+        <b>Week 1</b>
+        {/* //Program Information 2023/2024 Edition */}
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+      <div class="accordion-body">
+<ul>
+    <li><a href="https://www.youtube.com/watch?v=kBT-bMNOy1g">About The Course</a></li>
+    </ul>
+      </div>
+    </div>
+  </div>
+  
+  
+  
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+<b>Week 2</b>
+{/* 100 Days of code */}
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
+      <div class="accordion-body">
+      <ul>
+    <li><a href="https://www.youtube.com/watch?v=lD1lk8YRXpw">pro Bootcamp</a></li>
+    </ul>
+      </div>
+    </div>
+  </div>
+ 
+ 
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+<b>Week 3</b>
+{/* Certified HTML5 Foundations 2023/2024 */}
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
+      <div class="accordion-body">
+      <ul>
+    <li><a href="https://www.youtube.com/watch?v=cHwgvngA5FA">HTML5 , CSS3  from  zero  to  expert - 2023</a></li>
+    <li><a href="https://www.youtube.com/watch?v=P0EGYTb1cBs">beginners for HTML5</a></li>
+    </ul>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+<b>Week 4</b>
+{/* Certified CSS Foundations 2023/2024 */}
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
+      <div class="accordion-body">
+      <ul>
+    <li><a href="https://www.google.com/search?sca_esv=600047669&rlz=1C1ONGR_enIN1020IN1020&sxsrf=ACQVn0_vQshd8LAl2PASF59U1Cn9z2Lkaw:1705748149382&q=CSS+%26+Javascript+certification+course+for+beginners+youtube+youAccel+training&tbm=vid&source=lnms&sa=X&ved=2ahUKEwjUqpSE5-uDAxVvTWwGHTtgDHAQ0pQJegQIDxAB&biw=1366&bih=617&dpr=1#fpstate=ive&vld=cid:e7739807,vid:YOkdQE947dk,st:0">Certification  Course  for  Beginners</a></li>
+    <li><a href="https://www.youtube.com/watch?v=PtYKcOuGalM">CSS  &  JavaScript for  Beginners</a></li>
+    <li><a href="https://www.youtube.com/watch?v=0PrcNQRkQ_c">Advanced Css and Sass</a></li>
+    </ul>
+    
+
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+<b>Week 5</b>
+{/* Certified JavaScript Foundations 2023/2024 */}
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
+      <div class="accordion-body">
+      <ul>
+    <li><a href="https://www.udemy.com/course/javascript-basics-to-advanced/">JavaScript Basics to Advanced  step  by  step</a></li>
+    <li><a href="https://www.udemy.com/course/learn-javascript-for-beginners-v/?utm_source=adwords-pmax&utm_medium=udemyads&utm_campaign=PMax_la.EN_cc.INDIA&utm_content=deal4584&utm_term=._ag._kw._ad._de_c.dm._pl._ti._li_9148884.pd.&gad_source=1&gclid=Cj0KCQiA-62tBhDSARIsAO7twbaBZ02awfsd2pbvM7qxs2G8vIzlt-1YXkRNwQm0BfSXBVYDpircfpoaAnglEALw_wcB">Learn JavaScript- For Beginners by YouAccel Training</a></li>
+    <li><a href="https://www.udemy.com/course/learn-javascript-from-beginner-to-advanced/?utm_source=adwords-pmax&utm_medium=udemyads&utm_campaign=PMax_la.EN_cc.INDIA&utm_content=deal4584&utm_term=._ag._kw._ad._de_c.dm._pl._ti._li_9148884.pd.&gad_source=1&gclid=Cj0KCQiA-62tBhDSARIsAO7twbbGjR0J0BHKZVA4CBq-B-3mjN-GVwt4exlh6maeSR4Yw4iQfMfX9E8aAlXeEALw_wcB">Complete JavaScript & jQuery Course with Bonus Vue JS Intro</a></li>
+    </ul>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+<b>Week 6</b>
+{/* Certified Python Foundations 2023/2024 */}
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
+      <div class="accordion-body">
+      <ul>
+    <li><a href="https://www.youtube.com/watch?v=YfO28Ihehbk">Python for Absolute Beginners python beginners to pro</a></li>
+    <li><a href="https://www.youtube.com/watch?v=KXTANzN1hfE">Master Programming and Development with 15 projects</a></li>
+    <li><a href="https://www.youtube.com/watch?v=kWEbNBXc2-Y">From Basics to Advanced Level</a></li>
+    </ul>
+
+      </div>
+    </div>
+  </div>
+
+
+
+
+</div>
+
+
+</div>
+    </>
+  );
+}
+
+
+export default Content;
